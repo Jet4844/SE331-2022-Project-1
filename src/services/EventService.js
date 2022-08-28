ae81a653063633ab50911f219360609dbaf57297
Alinjx <chanchanok.2544@gmail.com>
2022-08-28 16:23:38 +0700

    firebase(1)

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL:
-    'https://my-json-server.typicode.com/Jet4844/SE331-2022-Project-1/peoples',
+  baseURL: process.env.VUE_APP_BACKEND_URL,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
